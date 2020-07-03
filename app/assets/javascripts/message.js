13b8f58b4ef9144f4119906c5e6c2b583937e660
rariruren555 <67192257+rariruren555@users.noreply.github.com>
2020-07-03 20:14:39 +0900

    Update Ajax

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,2 +1,61 @@
 $(function(){
+  function buildHTML(message){
+    if (message.image) {
+      let html =
+        `<div class="messagebox">
+          <div class="messagebox__namedate">
+            <div class="messagebox__namedate__name">
+              ${message.user_name}
+            </div>
+            <div class="messagebox__namedate__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <p class="Message__content">
+            ${message.content}
+          </p>
+          <div class="messagebox__text">
+            <img class="Message__image" src="${message.image}">
+          </div>
+        </div>`
+      return html;
+    } else {
+      let html =
+      `<div class="messagebox">
+         <div class="messagebox__namedate">
+          <div class="messagebox__namedate__name">
+            ${message.user_name}
+          </div>
+          <div class="messagebox__namedate__date">
+           ${message.created_at}
+          </div>
+        </div>
+        <div class="messagebox__text">
+          <p class="Message__content">
+            ${message.content}
+          </p>
+        </div>
+      </div>`
+      return html;
+    };
+  }
+
+  $('.content').on('submit', function(e){
+    e.preventDefault()
+    let formData = new FormData(this)
+    let url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      let html = buildHTML(data);
+      $('.chat-main__message-list').append(html);
+      $('form')[0].reset();
+    })
+  });
 });
